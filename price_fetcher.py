e009f659aa4bb67786b73137e16c4a6e62ffe4f9
Hitesh Gulati <hg0111@gmail.com>
2018-07-19 18:18:32 +0530

    update

--- price_fetcher.py
+++ price_fetcher.py
@@ -1,10 +1,10 @@
-# import quandl
+import quandl
 from datetime import datetime
 from datetime import timedelta
 import pandas as pd
-# quandl.ApiConfig.api_key = 'NjFyz5PTC_XMypj9dL9f'
+quandl.ApiConfig.api_key = 'NjFyz5PTC_XMypj9dL9f'
 ticker = "IBULISL"
-database = "NSE"
+database = "NSE/"
 quandl_code = database + "/" + ticker
 yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 print(yesterday)
@@ -12,8 +12,7 @@ fetch_date = yesterday
 # quandl.get(quandl_code, start_date = fetch_date, end_date = fetch_date).Close[0]
 price_file = pd.read_excel("Price Reporter.xlsx", sheet_name = "Input")
 
-for i, items in price_file.iterrows():
-    print(items.NSE_Ticker)
+
 slice = {"NSE_Ticker":"", "Remarks":"", "DOA": datetime(1,1,1), "DOA_Open":0,"DOA_Close":0,
          "Date1": datetime(1, 1, 1), "Date1_Open": 0, "Date1_Close": 0,
          "Date2": datetime(1, 1, 1), "Date2_Open": 0, "Date2_Close": 0,
@@ -26,5 +25,13 @@ slice = {"NSE_Ticker":"", "Remarks":"", "DOA": datetime(1,1,1), "DOA_Open":0,"DO
          "Date9": datetime(1, 1, 1), "Date9_Open": 0, "Date9_Close": 0,
          "Date10": datetime(1, 1, 1), "Date10_Open": 0, "Date10_Close": 0}
 outputdf = pd.DataFrame(columns=list(slice))
+for i, item in price_file.iterrows():
+    slice['NSE_Ticker'] = item.NSE_Ticker
+    slice['DOA'] = item.Date_of_Action
+    quandl_code = database + slice['NSE_Ticker']
+    fetch_date = slice['DOA'].strftime('%Y-%m-%d')
+    slice['DOA_Open'] = quandl.get(quandl_code, start_date = fetch_date, end_date = fetch_date).Open[0]
+    slice['DOA_Close'] = quandl.get(quandl_code, start_date=fetch_date, end_date=fetch_date).Close[0]
 
+    print(item.NSE_Ticker)
 
